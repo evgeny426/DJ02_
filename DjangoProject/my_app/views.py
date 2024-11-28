0832dcf20145113d47262316b2d4a54d0c908ee1
evgeny426 <bardygin1@yandex.ru>
2024-11-28 16:09:11 +0400

    Done

---
+++ DjangoProject/my_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    data = {
+        'caption': 'ShrekDjango'
+    }
+    return render(request, 'my_app/index.html', data)
+
+def data(request):
+    return render(request, 'my_app/data.html')
+
+def test(request):
+    return render(request, 'my_app/test.html')
+
+def four(request):
+    return render(request, 'my_app/four.html')
